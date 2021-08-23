0586ab441f229c920e8cea01e847a965388da2d6
Ray Villalobos <ray@planetoftheweb.com>
2021-08-23 19:59:53 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './**/*.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
